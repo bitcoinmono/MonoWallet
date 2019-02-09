01f4ed2c88f6fd22f061690c0589ca3d2f6109a7
Learner <linhdqtg@gmail.com>
2019-02-10 02:59:06 +0700

    Rebrand
    BitcoinMono config

--- src/js/ws_config.js
+++ src/js/ws_config.js
@@ -1,20 +1,20 @@
 var config = {};
 
 // self explanatory, your application name, descriptions, etc
-config.appName = 'GoldWallet';
-config.appDescription = 'DeroGold Wallet';
+config.appName = 'MonoWallet';
+config.appDescription = 'BitcoinMono Wallet';
 config.appSlogan = 'The Litecoin to TurtleCoin\'s Bitcoin';
-config.appId = 'gold.wallet.dego';
-config.appGitRepo = 'https://github.com/ExtraHash/dego-wallet-electron';
+config.appId = 'io.bitcoinmono.walletshell';
+config.appGitRepo = 'https://github.com/bitcoinmono/MonoWallet';
 
 // default port number for your daemon (e.g. TurtleCoind)
-config.daemonDefaultRpcPort = 6969;
+config.daemonDefaultRpcPort = 11358;
 
 // wallet file created by this app will have this extension
 config.walletFileDefaultExt = 'wallet';
 
 // change this to match your wallet service executable filename
-config.walletServiceBinaryFilename = 'DeroGold-service';
+config.walletServiceBinaryFilename = 'BitcoinMono-service';
 
 // version on the bundled service (turtle-service)
 config.walletServiceBinaryVersion = "v0.0.4";
@@ -25,13 +25,13 @@ config.walletServiceBinaryVersion = "v0.0.4";
 config.walletServiceConfigFormat = "json";
 
 // default port number for your wallet service (e.g. turtle-service)
-config.walletServiceRpcPort = 1337;
+config.walletServiceRpcPort = 11359;
 
 // block explorer url, the [[TX_HASH]] will be substituted w/ actual transaction hash
-config.blockExplorerUrl = '';
+config.blockExplorerUrl = 'explorer.bitcoinmono.io';
 
 // default remote node to connect to, set this to a known reliable node for 'just works' user experience
-config.remoteNodeDefaultHost = 'goldminer.tk';
+config.remoteNodeDefaultHost = 'node.btcm.semipool.com';
 
 // remote node list update url, set to null if you don't have one
 config.remoteNodeListUpdateUrl = '';
@@ -42,11 +42,11 @@ config.remoteNodeListFallback = [
 ];
 
 // your currency name
-config.assetName = 'DeroGold';
+config.assetName = 'BitcoinMono';
 // your currency ticker
-config.assetTicker =  'DEGO';
+config.assetTicker =  'BTCM';
 // your currency address prefix, for address validation
-config.addressPrefix =  'dg';
+config.addressPrefix =  'btcm';
 // standard wallet address length, for address validation
 config.addressLength = 99;
 // intergrated wallet address length, for address validation
@@ -59,9 +59,9 @@ config.mininumSend = 0.1;
 // default mixin/anonimity for transaction
 config.defaultMixin = 3;
 // to convert from atomic unit
-config.decimalDivisor = 100;
+config.decimalDivisor = 10000;
 // to represent human readable value
-config.decimalPlaces = 2;
+config.decimalPlaces = 4;
 
 // obfuscate address book entries, set to false if you want to save it in plain json file.
 // not for security because the encryption key is attached here
@@ -72,7 +72,8 @@ config.addressBookObfuscationKey = '79009fb00ca1b7130832a42de45142cf6c4b7f333fe6
 config.addressBookSampleEntries = [
     { name: 'ExtraHash',
       address: 
-'dg4weDyL6UNLvymc1M2RAPVcyHy2SGVFMQaaryiuqGvTR6F83Wg2kRbGRAy4SfomvWG67gwkieGnqc6baXx9DqbN2cbskdAxS',
+'btcmzR7v3pfW25Ddz26pR1N5Jt65sVVt4iFt4eY3862FUgWzM58U5CVfkfRQH6UjVvbvvQ9yaV6KK7hiNMc52Z1zVjqWsjXdify
+',
       paymentId: 'DF794857BC4587ECEC911AF6A6AB02513FEA524EC5B98DA8702FAC92195A94B2', 
     }
 ];
